494bc401f708308804cbd0c513ddd8e69a27ff5c
kimsong <saokimsong@gmail.com>
2023-05-26 09:40:44 +0700

    Demo Week 9

--- app/src/main/java/kh/edu/rupp/fe/visitme/MainActivity.java
+++ app/src/main/java/kh/edu/rupp/fe/visitme/MainActivity.java
@@ -1,14 +1,28 @@
 package kh.edu.rupp.fe.visitme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.os.LocaleListCompat;
 
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_image);
+        setContentView(R.layout.activity_main);
+//        LocaleListCompat appLocale = LocaleListCompat.forLanguageTags("km-KH");
+//        // Call this on the main thread as it may require Activity.restart()
+//        AppCompatDelegate.setApplicationLocales(appLocale);
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
+
 }
